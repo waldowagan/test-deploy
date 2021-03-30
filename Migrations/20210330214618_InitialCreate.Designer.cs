af5e2485495b68a1d97f64d98a7459da959d7a41
Ubuntu <test-admin@staging-vm.2rl3fbgud4xezjgfgxumjp0l4g.bx.internal.cloudapp.net>
2021-03-30 21:48:32 +0000

    initial commit

--- Migrations/20210330214618_InitialCreate.Designer.cs
+++ Migrations/20210330214618_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetCoreSqlDb.Migrations
 {
     [DbContext(typeof(MyDatabaseContext))]
-    [Migration("20210330211639_InitialCreate")]
+    [Migration("20210330214618_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
